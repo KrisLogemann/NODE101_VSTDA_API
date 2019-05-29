8dfd91e3c43977ec0dae635a9e9b516064396800
Kris Logemann <krisrlogemann@gmail.com>
2019-05-29 10:29:18 -0700

    adds initial project files

---
+++ README.md
@@ -0,0 +1,7 @@
+Created May 29, 2019 10:25 by Kris Logemann
+
+This project will be used to set up a back end server which you can use in later projects to build the front end. It will help to get into the habit of the concept known as the "contract" in which front and back end developers can work in parallel of each other by having a given application and its api behave a certain way during the process for better efficiancy.
+
+Thanks for checking out my code on GitHub.
+
+Kris Logemann
--- server/app.js
+++ server/app.js
@@ -1,8 +1,72 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// add your code here
+var todoItems = [
+    {
+      todoItemId: 0,
+      name: 'an item',
+      priority: 3,
+      completed: false
+    },
+    {
+      todoItemId: 1,
+      name: 'another item',
+      priority: 2,
+      completed: false
+    },
+    {
+      todoItemId: 2,
+      name: 'a done item',
+      priority: 1,
+      completed: true
+    }
+];
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send(todoItems);
+});
+
+app.get('/api/TodoItems', (req, res) => {
+    res.status(200).send(todoItems);
+});
+
+app.get('/api/TodoItems/:number', (req, res) => {
+    let id = parseInt(req.params.number);
+    for (let i = 0; i < todoItems.length; i++) {
+        let todo = todoItems[i];
+        if (todo.todoItemId === id) {
+            res.json(todo);
+            return;
+        }
+    }
+});
+
+app.post('/api/TodoItems', (req, res) => {
+    let newTodo = parseInt(req.body.todoItemId)
+    for (let i = 0; i < todoItems.length; i++)  {
+        if (newTodo === todoItems[i].todoItemId) {
+            todoItems[i] = req.body;
+        } else {
+            todoItems.push(req.body);
+        }
+    }
+    res.status(201).json(req.body);
+});
+
+app.delete('/api/TodoItems/:number', (req, res) => {
+    let id = parseInt(req.params.number)
+    for (let i = 0; i < todoItems.length; i++) {
+        if (id === todoItems[i].todoItemId) {
+            let item = todoItems.splice(i,1);
+            return res.json(item[0]);
+        }
+    }
+    res.status(200).send();
+});
 
 module.exports = app;
